7a8c23e0c69dfbe014ad0596adaec63323ce088c
jackrlynn <jacklynn@uiowa.edu>
2020-10-20 17:07:54 -0500

    Unzipped File

---
+++ Assets/Scripts/HighScores.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScores : MonoBehaviour {
+
+    public Text highScoreName1;
+    public Text highScoreName2;
+    public Text highScoreName3;
+    public Text highScoreName4;
+    public Text highScoreName5;
+
+    public Text highScore1;
+    public Text highScore2;
+    public Text highScore3;
+    public Text highScore4;
+    public Text highScore5;
+
+    //playerNameInput refers to a text box which is part of an InputField UI element
+    public Text playerNameInput;
+    string playerName;
+
+    //this value represents which rank the Player's score attained
+    int entryChanged;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        //PlayerPrefs.DeleteAll();
+
+        entryChanged = PlayerPrefs.GetInt("High Score Entry");
+         
+        //initial setting of high score UI text
+        highScore1.text = PlayerPrefs.GetInt("High Score1").ToString();
+        highScore2.text = PlayerPrefs.GetInt("High Score2").ToString();
+        highScore3.text = PlayerPrefs.GetInt("High Score3").ToString();
+        highScore4.text = PlayerPrefs.GetInt("High Score4").ToString();
+        highScore5.text = PlayerPrefs.GetInt("High Score5").ToString();
+
+        highScoreName1.text = PlayerPrefs.GetString("High Score Name1");
+        highScoreName2.text = PlayerPrefs.GetString("High Score Name2");
+        highScoreName3.text = PlayerPrefs.GetString("High Score Name3");
+        highScoreName4.text = PlayerPrefs.GetString("High Score Name4");
+        highScoreName5.text = PlayerPrefs.GetString("High Score Name5");
+
+
+        UpdateHighScoreNames();
+    }
+
+    void Update() {
+
+        playerName = playerNameInput.text;
+        UpdateHighScoreNames();
+    }
+
+    //checks the value of entryChanged and changes the value of the corresponding
+    //high score name to playerName
+    void UpdateHighScoreNames() {
+
+        if (entryChanged == 1) {
+            highScoreName1.text = playerName;
+            PlayerPrefs.SetString("High Score Name1", playerName);
+        }else if (entryChanged == 2) {
+            highScoreName2.text = playerName;
+            PlayerPrefs.SetString("High Score Name2", playerName);
+        }else if (entryChanged == 3) {
+            highScoreName3.text = playerName;
+            PlayerPrefs.SetString("High Score Name3", playerName);
+        }else if (entryChanged == 4) {
+            highScoreName4.text = playerName;
+            PlayerPrefs.SetString("High Score Name4", playerName);
+        }else if (entryChanged == 5) {
+            highScoreName5.text = playerName;
+            PlayerPrefs.SetString("High Score Name5", playerName);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour {
+
+    float fall = 0;
+    //maximum time between each downward movement is set to 1 second
+    public float fallSpeed = 1;
+    //play area is a 10x20 grid with (0,0) origin in bottom left
+    int gridWidth = 10;
+
+    public bool allowRotation = true;
+    public bool limitRotation = false; 
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        CheckUserInput();
+    }
+
+    void CheckUserInput() {
+
+        //checks for input to move tetromino in specified direction
+        //up arrow rotates the object
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            transform.position += new Vector3(-1, 0);
+
+            if (CheckIsValidPosition()) {
+                FindObjectOfType<Game>().UpdateGrid(this);
+            }else {
+                transform.position += new Vector3(1, 0);
+            }
+        }else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            transform.position += new Vector3(1, 0);
+
+            if (CheckIsValidPosition()) {
+                FindObjectOfType<Game>().UpdateGrid(this);
+            }else {
+                transform.position += new Vector3(-1, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            //types of rotating allowed depends on tetromino type
+            if (allowRotation) {
+                if (limitRotation) {
+                    if (transform.rotation.eulerAngles.z >= 90) {
+                        transform.Rotate(0, 0, -90);
+                    }else {
+                        transform.Rotate(0, 0, 90); 
+                    }
+                }else {
+                    transform.Rotate(0, 0, 90);
+                }
+
+                if (CheckIsValidPosition()) {
+                    FindObjectOfType<Game>().UpdateGrid(this);
+                }
+                else {
+                    transform.Rotate(0, 0, -90);
+                }
+            }
+
+        }else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed) {
+            transform.position += new Vector3(0, -1);
+
+            //we can use the else condition to check if the tetromino has reached its
+            //final position
+            if (CheckIsValidPosition()) {
+                FindObjectOfType<Game>().UpdateGrid(this);
+            }
+            else {
+                transform.position += new Vector3(0, 1);
+
+                FindObjectOfType<Game>().DeleteRow();
+
+                //if the tetromino's final position is at the top -> game over 
+                if (FindObjectOfType<Game>().CheckIsAboveGrid(this)) {
+                    FindObjectOfType<Game>().GameOver(); 
+                }
+
+                enabled = false;
+                FindObjectOfType<Game>().SpawnNextTetromino();
+            }
+            fall = Time.time; 
+        }
+    }
+
+    //checks if tetromino is in bounds and not occupying another tetromino's position
+    bool CheckIsValidPosition() {
+
+        foreach (Transform mino in transform) {
+            Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
+            if ((int)pos.x < 0 || (int)pos.x >= gridWidth || (int)pos.y < 0) {
+                return false;
+            }
+
+            if (FindObjectOfType<Game>().GetTransformAtGridPosition(pos) != null 
+                && FindObjectOfType<Game>().GetTransformAtGridPosition(pos).parent != transform) {
+                return false; 
+            }
+        }
+
+        return true;
+    }
+}
